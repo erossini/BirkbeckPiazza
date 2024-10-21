fee2566e224e930121be372e427dc5fb3b988a9e
Enrico Rossini <enrico.rossini.uk@live.com>
2024-10-21 10:17:19 +0100

    Swagger update

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,55 @@ const router = express.Router();
 
 const User = require('../models/user')
 
+router.delete('/:userId', async (req, res) => {
+    /*
+        #swagger.tags = ['Users']
+        #swagger.path = '/api/users/{userId}'
+        #swagger.summary = 'Delete user by userId'
+        #swagger.description = 'Delete a user by the userId from the collection'
+        #swagger.parameters['userId'] = { description: 'This is the UserId in the MongoDB collection' }
+    */
+    const userToDelete = await User.findById(req.params.userId);
+    if (!userToDelete)
+        return res.status(404).json({ error: 'User not found' });
+
+    await User.deleteOne(userToDelete);
+    res.status(200).json({ error: 'User deleted' });
+})
+
+router.delete('/username/:username', async (req, res) => {
+    /*
+        #swagger.tags = ['Users']
+        #swagger.path = '/api/users/username/{username}'
+        #swagger.summary = 'Delete user by username'
+        #swagger.description = 'Delete a user by the userId from the collection'
+        #swagger.parameters['username'] = { description: 'This is the username for the user to delete in the MongoDB collection' }
+    */
+    const userToDelete = await User.findOne({ username: req.params.username });
+    if (!userToDelete)
+        return res.status(404).json({ error: 'User not found' });
+
+    await User.deleteOne(userToDelete);
+    res.status(200).json({ error: 'User deleted' });
+})
+
+
+router.delete('/email/:email', async (req, res) => {
+    /*
+        #swagger.tags = ['Users']
+        #swagger.path = '/api/users/email/{email}'
+        #swagger.summary = 'Delete user by email'
+        #swagger.description = 'Delete a user by the email from the collection'
+        #swagger.parameters['username'] = { description: 'This is the email for the user to delete in the MongoDB collection' }
+    */
+    const userToDelete = await User.findOne({ email: req.params.email });
+    if (!userToDelete)
+        return res.status(404).json({ error: 'User not found' });
+
+    await User.deleteOne(userToDelete);
+    res.status(200).json({ error: 'User deleted' });
+})
+
 router.get('/', async (req, res) => {
     /*
        #swagger.tags = ['Users']
@@ -12,11 +61,14 @@ router.get('/', async (req, res) => {
     */
     try {
         const list = await User.find();
-        res.send(list);
+        if(!list || list.length == 0)
+            res.status(404).json({ error: 'No users found' });
+
+        res.status(200).send(list);
     }
     catch(err) {
         console.log('Error retrieving the users: ', err);
-        res.send('Error retrieving the users: ', err);
+        res.status(500).json({ error: 'Error retrieving the users: ', details: err });
     }
 })
 
@@ -25,15 +77,19 @@ router.get('/:userId', async (req, res) => {
        #swagger.tags = ['Users']
        #swagger.path = '/api/users/{userId}'
        #swagger.summary = 'Get a user by userId'
-       #swagger.description = 'Get all the users from the collection'
+       #swagger.description = 'Get a user by the UserId from the collection'
        #swagger.parameters['userId'] = { description: 'This is the UserId in the MongoDB collection' }
     */
     try {
         const userSearchById = await User.findById(req.params.userId);
-        res.send(userSearchById);
+        if(!userSearchById)
+            return res.status(404).json({ error: 'User not found' });
+
+        res.status(200).send(userSearchById);
     }
     catch(err) {
         console.log('User not found: ', err);
+        res.status(404).json({ error: 'UserId not found' });
     }
 });
 
@@ -53,16 +109,24 @@ router.post('/register', async (req, res) => {
             }
     } */
 
+    const email = req.body.email;
+    if(!email)
+        return res.status(400).json({ error: 'The email is required.' });
+
+    const userToVerify = await User.findOne({ email: email });
+    if(userToVerify)
+        res.status(404).json({ error: 'This email is already in use.'});
+
     // prepare the data to save in the database
     const user = new User({
         username: req.body.username,
-        email: req.body.email,
+        email: email,
         password: req.body.password
     });
 
     // save the user in the database and return the saved user
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.status(200).send(savedUser);
 })
 
 router.get('/searchByEmail/:email', async(req, res) => {
@@ -70,10 +134,10 @@ router.get('/searchByEmail/:email', async(req, res) => {
       #swagger.tags = ['Users']
       #swagger.path = '/api/users/searchByEmail/{email}'
       #swagger.summary = 'Get a user by email'
+      #swagger.description = 'Search a user by the email'
     */
-    console.log(req.params.email);
     const userSearchByEmail = await User.findOne({ email: req.params.email});
-    res.send(userSearchByEmail);
+    res.status(200).send(userSearchByEmail);
 })
 
 module.exports = router;
